5d27868cd2374b694c4ebfb029a8b8dcb9243c24
Alfan Adi Chandra <46060522+alfancandra@users.noreply.github.com>
2021-07-27 00:37:58 +0700

    update

--- admin/theme/templates.php
+++ admin/theme/templates.php
@@ -403,5 +403,5 @@ $('#date_picker').datetimepicker({
   
   
 </body> 
-      <footer><p  style="text-align: center;font-weight: bold;">Copyright &copy; Bachelor of Science and Entrepreneurs </p></footer>
+      <footer><p  style="text-align: center;font-weight: bold;">AkunGame </p></footer>
 </html>
